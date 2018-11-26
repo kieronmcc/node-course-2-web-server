58ae3a0861eeadb97d4dedb5ff30dd622e7d7810
Kieron McCarthy <kieronmcc@gmail.com>
2018-11-26 15:51:11 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require ('express');
+const hbs = require('hbs');
+const fs = require ('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append ro server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  //response.send('<h1>Hello Express!</h1>');
+  response.render('home.hbs',{
+    welcomeText: 'Welcome to the HBS website',
+    pageTitle: 'Home Page',
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// /bad send back json with errormessage property
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Unable to fulfill request'
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is now runnning on port 3000');
+});

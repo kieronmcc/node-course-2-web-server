45477306a21211891cdf3fda203dfdbe9eed6f34
Kieron McCarthy <kieronmcc@gmail.com>
2018-11-27 11:13:23 +0100

    Changes for Heroku start and port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const hbs = require('hbs');
 const fs = require ('fs');
-
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -57,6 +57,6 @@ app.get('/bad', (request, response) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is now runnning on port 3000');
+app.listen(port, () => {
+  console.log(`Server is now runnning on port ${port}`);
 });
